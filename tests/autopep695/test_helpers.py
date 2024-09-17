a594fb8da5ef3b2171cc761810f650c98b3fae9b
yowoda <yodarlswitch@gmail.com>
2024-09-17 17:37:37 +0200

    Add copyright notice

--- tests/autopep695/test_helpers.py
+++ tests/autopep695/test_helpers.py
@@ -1,3 +1,8 @@
+# Copyright (c) 2024-present yowoda
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
 import libcst as cst
 import pytest
 
