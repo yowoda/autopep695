85dec46de089d52541ce2d36f81e6ca6cd028622
yowoda <yodarlswitch@gmail.com>
2024-09-17 13:27:42 +0200

    Better lockfile

--- noxfile.py
+++ noxfile.py
@@ -4,6 +4,7 @@
 # LICENSE file in the root directory of this source tree.
 
 import os
+import typing as t
 
 import nox
 from nox import options
@@ -16,14 +17,26 @@ SCRIPT_PATHS = [
 options.sessions = ["format_fix", "typecheck", "slotscheck"]
 
 
-@nox.session()
+def session(
+    **kwargs: t.Any,
+) -> t.Callable[[t.Callable[[nox.Session], None]], t.Callable[[nox.Session], None]]:
+    kwargs.setdefault("venv_backend", "uv|virtualenv")
+    kwargs.setdefault("reuse_venv", True)
+
+    def inner(func: t.Callable[[nox.Session], None]) -> t.Callable[[nox.Session], None]:
+        return nox.session(**kwargs)(func)
+
+    return inner
+
+
+@session()
 def format_fix(session: nox.Session) -> None:
     session.install(".[dev.format]")
     session.run("python", "-m", "ruff", "format", *SCRIPT_PATHS)
     session.run("python", "-m", "ruff", "check", "--fix", *SCRIPT_PATHS)
 
 
-@nox.session()
+@session()
 def format_check(session: nox.Session) -> None:
     session.install(".[dev.format]")
     session.run("python", "-m", "ruff", "format", *SCRIPT_PATHS, "--check")
@@ -32,13 +45,13 @@ def format_check(session: nox.Session) -> None:
     )
 
 
-@nox.session()
+@session()
 def typecheck(session: nox.Session) -> None:
     session.install(".[dev.typecheck]")
     session.run("python", "-m", "pyright")
 
 
-@nox.session()
+@nox.session(reuse_venv=True)
 def slotscheck(session: nox.Session) -> None:
     session.install(".[dev.slotscheck]")
     session.run("python", "-m", "slotscheck", "-m", "autopep695")
